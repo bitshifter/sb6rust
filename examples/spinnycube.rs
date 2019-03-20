0d4a973473e2de9d8aea8d56c0e58c0b9725a5cc
Cameron Hart <cameron.hart@gmail.com>
2019-03-20 21:55:58 +1100

    Swtich over completely to glam.

--- examples/spinnycube.rs
+++ examples/spinnycube.rs
@@ -192,14 +192,14 @@ impl sb6::App for SampleApp {
             gl::UniformMatrix4fv(self.proj_location, 1, gl::FALSE, self.proj_matrix.as_ptr());
 
             let f = time as f32 * 0.3;
-            let mv_matrix = vmath::translate(0.0, 0.0, -4.0)
+            let mv_matrix = vmath::rotate(time as f32 * 81.0, 1.0, 0.0, 0.0)
+                * vmath::rotate(time as f32 * 45.0, 0.0, 1.0, 0.0)
                 * vmath::translate(
                     (2.1 * f).sin() * 0.5,
                     (1.7 * f).cos() * 0.5,
                     (1.3 * f).sin() * (1.5 * f).cos() * 2.0,
                 )
-                * vmath::rotate(time as f32 * 45.0, 0.0, 1.0, 0.0)
-                * vmath::rotate(time as f32 * 81.0, 1.0, 0.0, 0.0);
+                * vmath::translate(0.0, 0.0, -4.0);
             gl::UniformMatrix4fv(self.mv_location, 1, gl::FALSE, mv_matrix.as_ptr());
 
             gl::DrawArrays(gl::TRIANGLES, 0, 36);
--- src/vmath.rs
+++ src/vmath.rs
@@ -22,190 +22,35 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-pub use glam::{vec3, vec4, Vec3, Vec4};
+pub use glam::{deg, vec3, vec4, Mat4, Vec3, Vec4};
 use std::f32;
-use std::fmt;
-use std::mem;
-use std::ops::Mul;
-
-#[inline]
-fn deg_to_rad(a: f32) -> f32 {
-    f32::consts::PI * 2.0 * a / 360.0
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct Mat4 {
-    pub col0: Vec4,
-    pub col1: Vec4,
-    pub col2: Vec4,
-    pub col3: Vec4,
-}
-
-#[allow(dead_code)]
-pub fn mat4(col0: Vec4, col1: Vec4, col2: Vec4, col3: Vec4) -> Mat4 {
-    Mat4 {
-        col0,
-        col1,
-        col2,
-        col3,
-    }
-}
 
 #[allow(dead_code)]
 pub fn perspective(fovy: f32, aspect: f32, near: f32, far: f32) -> Mat4 {
-    let q = 1.0 / deg_to_rad(0.5 * fovy).tan();
-    let a = q / aspect;
-    let b = (near + far) / (near - far);
-    let c = (2.0 * near * far) / (near - far);
-
-    Mat4 {
-        col0: vec4(a, 0.0, 0.0, 0.0),
-        col1: vec4(0.0, q, 0.0, 0.0),
-        col2: vec4(0.0, 0.0, b, -1.0),
-        col3: vec4(0.0, 0.0, c, 0.0),
-    }
+    Mat4::perspective(deg(fovy), aspect, near, far)
 }
 
 #[allow(dead_code)]
 pub fn translate(x: f32, y: f32, z: f32) -> Mat4 {
-    Mat4 {
-        col0: vec4(1.0, 0.0, 0.0, 0.0),
-        col1: vec4(0.0, 1.0, 0.0, 0.0),
-        col2: vec4(0.0, 0.0, 1.0, 0.0),
-        col3: vec4(x, y, z, 1.0),
-    }
+    Mat4::from_translation(vec3(x, y, z))
 }
 
 #[allow(dead_code)]
 pub fn look_at(eye: Vec3, center: Vec3, up: Vec3) -> Mat4 {
-    let f = (center - eye).normalize();
-    let up_n = up.normalize();
-    let s = f.cross(up_n);
-    let u = s.cross(f);
-    Mat4 {
-        col0: vec4(s.get_x(), u.get_x(), -f.get_x(), 0.0),
-        col1: vec4(s.get_y(), u.get_y(), -f.get_y(), 0.0),
-        col2: vec4(s.get_z(), u.get_z(), -f.get_z(), 0.0),
-        col3: vec4(-s.dot(eye), -u.dot(eye), f.dot(eye), 1.0),
-    }
+    Mat4::look_at(eye, center, up.normalize())
 }
 
 #[allow(dead_code)]
 pub fn rotate(angle: f32, x: f32, y: f32, z: f32) -> Mat4 {
-    let x2 = x * x;
-    let y2 = y * y;
-    let z2 = z * z;
-    let rads = deg_to_rad(angle);
-    let (sin, cos) = rads.sin_cos();
-    let omc = 1.0 - cos;
-    Mat4 {
-        col0: vec4(
-            x2 * omc + cos,
-            y * x * omc + z * sin,
-            x * z * omc - y * sin,
-            0.0,
-        ),
-        col1: vec4(
-            x * y * omc - z * sin,
-            y2 * omc + cos,
-            y * z * omc + x * sin,
-            0.0,
-        ),
-        col2: vec4(
-            x * z * omc + y * sin,
-            y * z * omc - x * sin,
-            z2 * omc + cos,
-            0.0,
-        ),
-        col3: vec4(0.0, 0.0, 0.0, 1.0),
-    }
+    Mat4::from_axis_angle(vec3(x, y, z), deg(angle))
 }
 
 #[allow(dead_code)]
 pub fn scale(x: f32, y: f32, z: f32) -> Mat4 {
-    Mat4 {
-        col0: vec4(x, 0.0, 0.0, 0.0),
-        col1: vec4(0.0, y, 0.0, 0.0),
-        col2: vec4(0.0, 0.0, z, 0.0),
-        col3: vec4(0.0, 0.0, 0.0, 1.0),
-    }
+    Mat4::from_scale(vec3(x, y, z))
 }
 
 #[allow(dead_code)]
 pub fn identity() -> Mat4 {
-    Mat4 {
-        col0: vec4(1.0, 0.0, 0.0, 0.0),
-        col1: vec4(0.0, 1.0, 0.0, 0.0),
-        col2: vec4(0.0, 0.0, 1.0, 0.0),
-        col3: vec4(0.0, 0.0, 0.0, 1.0),
-    }
-}
-
-#[allow(dead_code)]
-impl Mat4 {
-    pub fn zero() -> Mat4 {
-        Mat4 {
-            col0: Vec4::zero(),
-            col1: Vec4::zero(),
-            col2: Vec4::zero(),
-            col3: Vec4::zero(),
-        }
-    }
-
-    #[inline]
-    pub unsafe fn as_ptr(&self) -> *const f32 {
-        std::mem::transmute(&self.col0)
-    }
-
-    pub fn store_to_slice(&self, slice: &mut [f32]) {
-        self.col0.store_to_slice(&mut slice[0..4]);
-        self.col1.store_to_slice(&mut slice[4..8]);
-        self.col2.store_to_slice(&mut slice[8..12]);
-        self.col3.store_to_slice(&mut slice[12..16]);
-    }
-}
-
-impl From<Mat4> for [f32; 16] {
-    fn from(m: Mat4) -> Self {
-        unsafe {
-            let mut out: [f32; 16] = mem::uninitialized();
-            m.col0.store_to_slice(&mut out[0..4]);
-            m.col1.store_to_slice(&mut out[4..8]);
-            m.col2.store_to_slice(&mut out[8..12]);
-            m.col3.store_to_slice(&mut out[12..16]);
-            out
-        }
-    }
-}
-
-impl Mul for Mat4 {
-    type Output = Mat4;
-    fn mul(self, rhs: Mat4) -> Mat4 {
-        let a0 = self.col0;
-        let a1 = self.col1;
-        let a2 = self.col2;
-        let a3 = self.col3;
-
-        let b0 = rhs.col0;
-        let b1 = rhs.col1;
-        let b2 = rhs.col2;
-        let b3 = rhs.col3;
-
-        Mat4 {
-            col0: (a0 * b0.get_x()) + (a1 * b0.get_y()) + (a2 * b0.get_z()) + (a3 * b0.get_w()),
-            col1: (a0 * b1.get_x()) + (a1 * b1.get_y()) + (a2 * b1.get_z()) + (a3 * b1.get_w()),
-            col2: (a0 * b2.get_x()) + (a1 * b2.get_y()) + (a2 * b2.get_z()) + (a3 * b2.get_w()),
-            col3: (a0 * b3.get_x()) + (a1 * b3.get_y()) + (a2 * b3.get_z()) + (a3 * b3.get_w()),
-        }
-    }
-}
-
-impl fmt::Display for Mat4 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "[{}, {}, {}, {}]",
-            self.col0, self.col1, self.col2, self.col3
-        )
-    }
+    Mat4::identity()
 }
